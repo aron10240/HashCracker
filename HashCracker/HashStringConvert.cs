d7e47dc0374adea876cfe553adaa5b4826279503
aron10240 <aronleopold02@gmail.com>
2026-07-20 09:20:43 +0200

    Projektdateien hinzufügen.

---
+++ HashCracker/HashStringConvert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HashCracker
+{
+    class HashStringConvert
+    {
+        public string StringToHash(string password)
+        {
+            SHA256 sha = SHA256.Create();
+            byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] hashBytes = sha.ComputeHash(textBytes);
+
+            string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+            return hash;
+        }
+    }
+}
---
+++ HashCracker/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using static HashCracker.HashStringConvert;
+
+namespace HashCracker
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HashStringConvert HSC = new HashStringConvert();
+            TBEncodedHash.Text = HSC.StringToHash(TBxInput.Text);
+        }
+    }
+}
