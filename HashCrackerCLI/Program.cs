ce08cf55ce9d3af49af8108ce0d7dc2d67959211
aron10240 <aronleopold02@gmail.com>
2026-07-30 18:53:55 +0200

    - HashCrackerCLI: Simple Brute-Force

--- HashCrackerCLI/Program.cs
+++ HashCrackerCLI/Program.cs
@@ -9,9 +9,11 @@ class Program
     static int Main(string[] args)
     {
         Option<string> dictionaryOption = new("--dictionary");
+        Option<string> simpleOption = new("--simple");
 
         RootCommand rootCommand = new("HashCrackerCLI");
         rootCommand.Options.Add(dictionaryOption);
+        rootCommand.Options.Add(simpleOption);
 
         ParseResult parseResult = rootCommand.Parse(args);
 
@@ -22,20 +24,33 @@ class Program
             return 1;
         }
 
-        if (parseResult.GetValue(dictionaryOption) is string input)
+        if (parseResult.GetValue(dictionaryOption) is string inputDictionary)
         {
             GetHashType getHashType = new GetHashType();
-            HashTyp hashtyp = getHashType.execute(input);
+            HashTyp hashtyp = getHashType.execute(inputDictionary);
 
             BruteForceDictionary bruteForceDictionary = new BruteForceDictionary();
-            string result = bruteForceDictionary.execute(input, hashtyp);
+            string result = bruteForceDictionary.execute(inputDictionary , hashtyp);
 
             Console.WriteLine("Hash: " + hashtyp);
             Console.WriteLine("Result: " + result);
             return 0;
         }
 
-        Console.Error.WriteLine("Please use --dictionary <hash>");
+        else if (parseResult.GetValue(simpleOption) is string inputSimple)
+        {
+            GetHashType getHashType = new GetHashType();
+            HashTyp hashtyp = getHashType.execute(inputSimple);
+
+            BruteForceSimple bruteForceSimple = new BruteForceSimple(inputSimple, hashtyp);
+            string result = bruteForceSimple.BruteForce();
+
+            Console.WriteLine("Hash: " + hashtyp);
+            Console.WriteLine("Result: " + result);
+            return 0;
+        }
+
+        Console.Error.WriteLine("Please use --dictionary <hash> or --simple <hash> ");
         return 1;
     }
 }
